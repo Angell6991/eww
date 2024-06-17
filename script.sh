aab362bb54123ecc3ad7fca67d48a767847ba37f
Angell69 <miguel9196.guzman@gmail.com>
2024-06-17 00:28:51 -0500

    config_bspwm_dek

--- script.sh
+++ script.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+# Obtener todos los nombres de los escritorios
+desktops=$(bspc query -D --names)
+
+# Inicializar la variable de salida
+status=""
+
+# Iterar sobre cada escritorio
+for desktop in $desktops; do
+    # Comprobar si el escritorio está ocupado
+    if bspc query -N -d $desktop > /dev/null; then
+        status="${status} 󰊠 "
+    else
+        status="${status}  "
+    fi
+done
+
+# Imprimir el estado formateado
+echo $status
 
