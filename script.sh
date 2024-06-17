b22d6b931f1cd692183e0f96e361d84d906268b3
Angell69 <miguel9196.guzman@gmail.com>
2024-06-17 00:48:21 -0500

    config_bspwm_dek_scrit

--- script.sh
+++ script.sh
@@ -3,17 +3,25 @@
 # Obtener todos los nombres de los escritorios
 desktops=$(bspc query -D --names)
 
+# Emojis para escritorios
+empty_emojis=("" "" "" "" "" "")
+occupied_emojis=(" " " " " " "󰴓 " "󰊠 " "󱍢 ")
+
 # Inicializar la variable de salida
 status=""
+
+# Contador de escritorios
+index=0
 
 # Iterar sobre cada escritorio
 for desktop in $desktops; do
     # Comprobar si el escritorio está ocupado
     if bspc query -N -d $desktop > /dev/null; then
-        status="${status} 󰊠 "
+        status=" ${status} ${occupied_emojis[$index]} "  # Emoji para ocupado
     else
-        status="${status}  "
+        status=" ${status} ${empty_emojis[$index]} "  # Emoji para vacío
     fi
+    ((index++))
 done
 
 # Imprimir el estado formateado
